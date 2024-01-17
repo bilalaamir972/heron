924f4d2326a14f29456a065cb55f08883f93c0c5
bilalaamir972 <bilalaamir41@gmail.com>
2024-01-17 23:02:37 +0500

    Update utils.py

--- heron/datasets/utils.py
+++ heron/datasets/utils.py
@@ -1,51 +1,28 @@
-# Copyright 2023 Turing Inc. Team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 from typing import Dict, Tuple
-
-import datasets
-import pandas as pd
-import yaml
 from torch.utils.data import ConcatDataset, Dataset
-
+from .custom_csv_dataset import CustomCSVDataset  # Replace with your custom dataset class
 from ..models.prepare_processors import get_processor
 from .ja_csv_datasets import JapaneseCSVDataset
 from .llava_datasets import LlavaDataset
 from .m3it_datasets import M3ITDataset
 
-
 def get_each_dataset(dataset_config: Dict, processor, max_length: int) -> Tuple[Dataset, Dataset]:
     if dataset_config["dataset_type"] == "m3it":
         train_dataset = M3ITDataset.create(dataset_config, processor, max_length, "train")
         val_dataset = M3ITDataset.create(dataset_config, processor, max_length, "validation")
-
     elif dataset_config["dataset_type"] == "japanese_csv":
         train_dataset = JapaneseCSVDataset.create(dataset_config, processor, max_length, "train")
-        val_dataset = JapaneseCSVDataset.create(
-            dataset_config, processor, max_length, "validation"
-        )
-
+        val_dataset = JapaneseCSVDataset.create(dataset_config, processor, max_length, "validation")
     elif dataset_config["dataset_type"] == "llava":
         train_dataset = LlavaDataset.create(dataset_config, processor, max_length, "train")
         val_dataset = LlavaDataset.create(dataset_config, processor, max_length, "validation")
-
+    elif dataset_config["dataset_type"] == "custom_csv":  # Add support for custom CSV dataset
+        train_dataset = CustomCSVDataset.create(dataset_config, processor, max_length, "train")
+        val_dataset = CustomCSVDataset.create(dataset_config, processor, max_length, "validation")
     else:
         raise ValueError(f"dataset_type: {dataset_config['dataset_type']} is not supported.")
 
     return train_dataset, val_dataset
-
 
 def get_dataset(config: Dict) -> Tuple[Dataset, Dataset]:
     processor = get_processor(config["model_config"])
@@ -54,11 +31,14 @@ def get_dataset(config: Dict) -> Tuple[Dataset, Dataset]:
     max_length = config["model_config"]["max_length"]
 
     for dataset_config_path in config["dataset_config_path"]:
-        with open(dataset_config_path, "r") as f:
-            dataset_config = yaml.safe_load(f)
-        train_dataset, val_dataset = get_each_dataset(dataset_config, processor, max_length)
-        train_dataset_list.append(train_dataset)
-        val_dataset_list.append(val_dataset)
+        try:
+            with open(dataset_config_path, "r") as f:
+                dataset_config = yaml.safe_load(f)
+            train_dataset, val_dataset = get_each_dataset(dataset_config, processor, max_length)
+            train_dataset_list.append(train_dataset)
+            val_dataset_list.append(val_dataset)
+        except IsADirectoryError:
+            print(f"Error: {dataset_config_path} is a directory, not a file.")
 
     train_dataset = ConcatDataset(train_dataset_list)
     val_dataset = ConcatDataset(val_dataset_list)
