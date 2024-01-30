e8966d1e8a7a5f33879a78a6b59d3f751d906638
bilalaamir972 <bilalaamir41@gmail.com>
2024-01-30 19:48:51 +0500

    Update my_custom_dataset.py

--- my_custom_dataset.py
+++ my_custom_dataset.py
@@ -50,38 +50,11 @@ class MyCSVDataset(BaseDataset):
         )
 
         return {
-            "input_ids": tokenized_text["input_ids"].squeeze(),
-            "labels": tokenized_text["input_ids"].squeeze(),
-            "attention_mask": tokenized_text["attention_mask"].squeeze(),
+            "input_ids": tokenized_text["input_ids"],
+            "labels": tokenized_text["input_ids"],
+            "attention_mask": tokenized_text["attention_mask"],
             "pixel_values": resized_image,
         }
 
 
-   def _get_item_inference(self, index):
-        row = self.data.iloc[index]
-        image_path = row["image_path"]
-        text = row["text"]
-
-        # Load image using PIL
-        image = Image.open(image_path).convert("RGB")
-
-        # Resize image
-        resized_image = self.image_transform(image)
-
-        # Tokenize text with a fixed max_length
-        tokenized_text = self.tokenizer(
-            text,
-            return_tensors="pt",
-            padding=True,
-            truncation=True,
-            max_length=512  # Specify a fixed max_length for tokenized sequences
-        )
-
-        return {
-            "input_ids": tokenized_text["input_ids"].squeeze(),
-            "labels": None,
-            "attention_mask": tokenized_text["attention_mask"].squeeze(),
-            "pixel_values": resized_image,
-        }
-
-
+   
